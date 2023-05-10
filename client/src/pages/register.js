8a702a524a80d65d775886a46614e13b1be1bd42
mayuripy <115979827+mayuripy@users.noreply.github.com>
2023-05-10 16:15:37 +0530

    .....

--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -1,31 +1,25 @@
 import React from 'react'
-import { Form,Input } from "antd";
+import { Form,Input, Typography } from "antd";
 
-const Register = () => {
+const Register = () =>{
 
-  const onfinishHandlar = (values) => {
-    console.log(values)
-  };
+  
 
   return (
-    <>
-       <div className='form-container'>
-        <h1>registration form</h1>
-        <Form layout="vertical" onfinish={onfinishHandlar}>
-           <Form.item label="Name" name="name">
-              <Input type="text" required />
-            </Form.item>
-            <Form.item label="Email" name="email">
-              <Input type="email" required />
-            </Form.item>
-            <Form.item label="password" name="password">
-              <Input type="password" required />
-            </Form.item>
-       <button className='btw btn-primary' type="submit">Register</button>
-        </Form>
-       </div>
-    </>
-  );
-};
-
-export default Register;
+    <Box 
+    flex={1}
+    display={'flex'}
+    flexdirection={'column'}
+    gap={'3vh'}
+    justifyContent={'center'}
+    alignItems={'center'}
+    
+    >
+      <Typography
+      fontFamily={'poppins'}
+      fontsize={'20px'}
+      >
+    Register
+      </Typography>
+       
+    </Box>
