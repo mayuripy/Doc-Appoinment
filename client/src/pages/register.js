e8c48c625578bcd5b83cea4b80d8644d5bcfc48b
mayuripy <115979827+mayuripy@users.noreply.github.com>
2023-05-13 23:35:50 +0530

    edit

--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -6,20 +6,30 @@ const Register = () =>{
   
 
   return (
-    <Box 
-    flex={1}
-    display={'flex'}
-    flexdirection={'column'}
-    gap={'3vh'}
-    justifyContent={'center'}
-    alignItems={'center'}
-    
-    >
-      <Typography
-      fontFamily={'poppins'}
-      fontsize={'20px'}
-      >
-    Register
-      </Typography>
-       
-    </Box>
+<>
+   <div className='form-container'>
+    <form layout="vertical" onfinish={onfinishHandler}>
+        <form.Item lable="Name" name="name">
+          <Input type="text" required/>
+          </form.Item>
+          <form.Item lable="Email name="email">
+          <Input type="email" required/>
+          </form.Item>
+          <form.Item lable="password" name="password">
+          <Input type="password" name="name"/>
+          </form.Item>
+          <form.Item lable="Name" name="name">
+          <Input type="text" name="name"/>
+          </form.Item>
+
+     </form>
+          
+        
+
+
+
+   
+   
+   
+   </div>
+</>
